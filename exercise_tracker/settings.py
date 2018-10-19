b544204d8e3df19bdd0d2c2e671882e01941a546
Daniel Jones <tortxof@gmail.com>
2018-10-18 20:34:05 -0400

    Add exercises app to config

--- exercise_tracker/settings.py
+++ exercise_tracker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'exercises.apps.ExercisesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
