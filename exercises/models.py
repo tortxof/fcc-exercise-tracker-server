0800f72e3c15595c39d40a6332cfb2356b965aab
Daniel Jones <tortxof@gmail.com>
2018-10-18 20:20:35 -0400

    Add ExerciseSession model

--- exercises/models.py
+++ exercises/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class ExerciseSession(models.Model):
+    description = models.CharField(max_length=255)
+    duration = models.IntegerField()
+    date = models.DateField(auto_now_add=True)
