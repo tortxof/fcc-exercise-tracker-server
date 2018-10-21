a921286e7a1ff01af66a1b109d7d76b51aa46078
Daniel Jones <tortxof@gmail.com>
2018-10-20 20:38:39 -0400

    Change duration field

--- exercises/models.py
+++ exercises/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class ExerciseSession(models.Model):
     description = models.CharField(max_length=255)
-    duration = models.IntegerField()
+    duration = models.PositiveIntegerField('Duration minutes')
     date = models.DateField(auto_now_add=True)
 
     def __str__(self):
