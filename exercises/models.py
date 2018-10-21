8b4124f8a4612ea92c457af3b0ee93c53c5f29ba
Daniel Jones <tortxof@gmail.com>
2018-10-20 20:51:42 -0400

    Change date field

--- exercises/models.py
+++ exercises/models.py
@@ -1,9 +1,11 @@
+from datetime import date
+
 from django.db import models
 
 class ExerciseSession(models.Model):
     description = models.CharField(max_length=255)
     duration = models.PositiveIntegerField('Duration minutes')
-    date = models.DateField(auto_now_add=True)
+    date = models.DateField(default=date.today)
 
     def __str__(self):
         return self.description
