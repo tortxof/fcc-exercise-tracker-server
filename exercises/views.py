9fab3f1b4f918f5e0b2768ed106972b9ec1b6c60
Daniel Jones <tortxof@gmail.com>
2018-10-22 00:00:48 -0400

    Add permissions

--- exercises/views.py
+++ exercises/views.py
@@ -1,14 +1,23 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import generics, viewsets, permissions
 
 from .models import ExerciseSession
 from .serializers import UserSerializer, ExerciseSessionSerializer
+from .permissions import IsOwnerOrReadOnly, IsSelfOrReadOnly
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsSelfOrReadOnly,
+    )
 
 class ExerciseSessionViewSet(viewsets.ModelViewSet):
     queryset = ExerciseSession.objects.all()
     serializer_class = ExerciseSessionSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+    )
