e53f83cca8ba6d29a8704f53c5376104553490ff
Daniel Jones <tortxof@gmail.com>
2018-10-22 02:36:10 -0400

    Adjust permissions

--- exercises/permissions.py
+++ exercises/permissions.py
@@ -4,12 +4,17 @@ class UserPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if view.action in ('list', 'create', 'retrieve'):
             return True
-
         return obj == request.user
 
 class ExerciseSessionPermission(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if view.action in ('list', 'create', 'retrieve'):
+    def has_permission(self, request, view):
+        if view.action in ('list', 'retrieve'):
             return True
-        
+
+        return view.action == 'create' and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if view.action in ('list', 'retrieve'):
+            return True
+
         return obj.user == request.user
