dea3c5d241e3d15dc00302ab48522519f55dfbd7
Daniel Jones <tortxof@gmail.com>
2018-10-25 15:50:21 -0400

    Config db from env

--- exercise_tracker/settings.py
+++ exercise_tracker/settings.py
@@ -84,12 +84,24 @@ WSGI_APPLICATION = 'exercise_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if os.getenv('DJANGO_DB_ENGINE') == 'postgresql':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('PG_NAME'),
+            'USER': os.getenv('PG_USER'),
+            'PASSWORD': os.getenv('PG_PASSWORD'),
+            'HOST': os.getenv('PG_HOST'),
+            'PORT': os.getenv('PG_PORT', default='5432'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
