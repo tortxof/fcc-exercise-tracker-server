b074b059c024057ef35fab75f5cb0455ee96bf0d
Daniel Jones <tortxof@gmail.com>
2018-10-25 14:42:38 -0400

    Fix permissions again

--- exercises/permissions.py
+++ exercises/permissions.py
@@ -1,8 +1,14 @@
 from rest_framework import permissions
 
 class ExerciseSessionPermission(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
         if view.action in ('list', 'retrieve'):
+            return True
+
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if view.action == 'retrieve':
             return True
 
         if view.action == 'create':
