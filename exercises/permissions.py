52dbdc06a4844c86e012f915863edd82e132645e
Daniel Jones <tortxof@gmail.com>
2018-10-25 09:39:51 -0400

    Fix exercise session permissions

--- exercises/permissions.py
+++ exercises/permissions.py
@@ -7,14 +7,11 @@ class UserPermission(permissions.BasePermission):
         return obj == request.user
 
 class ExerciseSessionPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if view.action in ('list', 'retrieve'):
-            return True
-
-        return view.action == 'create' and request.user.is_authenticated
-
     def has_object_permission(self, request, view, obj):
         if view.action in ('list', 'retrieve'):
             return True
+
+        if view.action == 'create':
+            return request.user.is_authenticated
 
         return obj.user == request.user
