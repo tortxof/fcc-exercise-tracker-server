ef3f59aedcf15ebaa92ce50e00ecc75d6df758f9
Daniel Jones <tortxof@gmail.com>
2018-10-27 12:30:30 -0400

    Add docs endpoint

--- exercise_tracker/urls.py
+++ exercise_tracker/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
 
 from exercises.views import UserViewSet, ExerciseSessionViewSet
 
@@ -29,4 +30,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('djoser.urls.authtoken')),
+    path('docs/', include_docs_urls(title='Exercise Tracker API')),
 ]
