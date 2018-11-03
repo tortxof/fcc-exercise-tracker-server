5a9e1098abdc1da4ea131f4685ef1bdff318fa6f
Daniel Jones <tortxof@gmail.com>
2018-11-03 12:32:54 -0400

    Store staticfiles manifest with app instance

--- exercise_tracker/storage.py
+++ exercise_tracker/storage.py
@@ -1,5 +1,25 @@
+from pathlib import Path
+import json
+
 from django.contrib.staticfiles.storage import ManifestFilesMixin
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(ManifestFilesMixin, S3Boto3Storage):
-    pass
+    def read_manifest(self):
+        try:
+            with open(self.manifest_name) as manifest:
+                return manifest.read()
+        except FileNotFoundError:
+            return None
+
+    def save_manifest(self):
+        if Path(self.manifest_name).is_file():
+            Path(self.manifest_name).unlink()
+        with open(self.manifest_name, 'w') as manifest:
+            json.dump(
+                {
+                    'paths': self.hashed_files,
+                    'version': self.manifest_version,
+                },
+                manifest,
+            )
